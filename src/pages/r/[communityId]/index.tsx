96c207f8e251b7d381a64fb24c126a566ad45f97
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-02 16:27:37 +0100

    updated commit

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,56 +1,71 @@
-import { Community } from "../../../atoms/communitiesAtom";
-import { firestore } from '@/src/firebase/clientApp';
-import { doc, getDoc } from 'firebase/firestore';
-import { GetServerSidePropsContext } from 'next';
-import React from 'react';
-import safeJsonStringify from "safe-json-stringify"
+import { Community, communityState } from "../../../atoms/communitiesAtom";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React, { useEffect } from "react";
+import safeJsonStringify from "safe-json-stringify";
 import NotFound from "@/src/components/Community/NotFound";
-import Header from "../../../components/Community/Header"
+import Header from "../../../components/Community/Header";
 import PageContent from "../../../components/layouts/PageContent";
 import CreatPostLink from "@/src/components/Community/CreatPostLink";
 import Post from "@/src/components/Post/Post";
+import { useSetRecoilState } from "recoil";
+import About from "@/src/components/Community/About";
 
 type Props = {
-    communityData:Community;
-}
+  communityData: Community;
+};
 
-const CommunityPage = ({communityData}:Props) => {
-    
-    if(!communityData){
-        return (
-         <>
-         <NotFound/>
-         </>
-        )
-    }
+const CommunityPage = ({ communityData }: Props) => {
+  const setCommunityStateValue = useSetRecoilState(communityState);
 
+  if (!communityData) {
+    return <NotFound />
+  
+  }
+
+  useEffect(()=>{
+    setCommunityStateValue(prev => ({
+      ...prev,
+      currentCommunity: communityData,
+    }))
+  }, [])
 
   return (
     <>
-    <Header communityData={communityData}/>
-    <PageContent>
-      <>
-      <CreatPostLink/>
-      <Post communityData={communityData}/>
-      </>
-      <>RHS</>
-    </PageContent>
+      <Header communityData={communityData} />
+      <PageContent>
+        <>
+          <CreatPostLink />
+          <Post communityData={communityData} />
+        </>
+        <>
+        <About communityData={communityData}/>
+        </>
+      </PageContent>
     </>
-  )
-}
-export async function getServerSideProps (context:GetServerSidePropsContext){
-    try {
-      const communityDocRef = doc(firestore, "communities", context.query.communityId as string) ;
-      const communityDoc = await getDoc(communityDocRef);
+  );
+};
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
 
-      return {
-        props:{
-            communityData:communityDoc.exists()? JSON.parse(safeJsonStringify({id:communityDoc.id, ...communityDoc.data()})
-            ) : "",
-        },
-      }
-    } catch (error) {
-        console.log("getServerSideProps error", error)
-    }
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
+      },
+    };
+  } catch (error) {
+    console.log("getServerSideProps error", error);
+  }
 }
-export default CommunityPage
+export default CommunityPage;
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,13 +1,18 @@
+import { communityState } from '@/src/atoms/communitiesAtom'
 import PageContent from '@/src/components/layouts/PageContent'
 import NewPostForm from '@/src/components/Post/NewPostForm'
 import { auth } from '@/src/firebase/clientApp'
 import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRecoilValue } from 'recoil'
 
 
 const SubmitPostPage = () => {
   const [user] = useAuthState(auth);
+  const communityStateValue = useRecoilValue(communityState);
+  console.log("COMMUNITY", communityStateValue);
+  
   return (
     <PageContent>
       <>
