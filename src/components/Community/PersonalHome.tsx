c0ed8085bd5f50e61e89f7b9f373769578b4bba9
BlackBond06 <eleazuosita007@yahoo.com>
2023-04-25 09:29:59 +0100

    updated commit

---
+++ src/components/Community/PersonalHome.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { Button, Flex, Icon, Stack, Text } from "@chakra-ui/react";
+import { FaReddit } from "react-icons/fa";
+import { User } from "firebase/auth";
+import CreateCommunityModal from "../Modal/Auth/CreateCommunity/CreateCommunityModal";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/src/atoms/authModalAtom";
+
+type Props = {
+  user?: User | null;
+};
+
+const PersonalHome: React.FC = ({ user }: Props) => {
+  const [open, setOpen] = useState(false);
+  const setAuthModalState = useSetRecoilState(authModalState);
+
+  return (
+    <>
+      <CreateCommunityModal open={open} handleClose={() => setOpen(false)} />
+      <Flex
+        direction="column"
+        bg="white"
+        borderRadius={4}
+        cursor="pointer"
+        border="1px solid"
+        borderColor="gray.300"
+        position="sticky"
+      >
+        <Flex
+          align="flex-end"
+          color="white"
+          p="6px 10px"
+          bg="blue.500"
+          height="34px"
+          borderRadius="4px 4px 0px 0px"
+          fontWeight={600}
+          bgImage="url(/images/redditPersonalHome.png)"
+          backgroundSize="cover"
+        ></Flex>
+        <Flex direction="column" p="12px">
+          <Flex align="center" mb={2}>
+            <Icon as={FaReddit} fontSize={50} color="brand.100" mr={2} />
+            <Text fontWeight={600}>Home</Text>
+          </Flex>
+          <Stack spacing={3}>
+            <Text fontSize="9pt">
+              Your personal Reddit frontpage, built for you.
+            </Text>
+            <Button height="30px">Create Post</Button>
+            <Button
+              variant="outline"
+              height="30px"
+              onClick={() =>
+                user
+                  ? setOpen(true)
+                  : setAuthModalState({ open: true, view: "login" })
+              }
+            >
+              Create Community
+            </Button>
+          </Stack>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
+export default PersonalHome;
